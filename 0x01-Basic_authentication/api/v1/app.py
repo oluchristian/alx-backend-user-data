b58a4d22a8fddef2110cef235609542322487a8f
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-08 06:53:35 +0100

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,18 +19,21 @@ if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request() -> Optional[str]:
     """before request
     """
-    allowed_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    allowed_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        ]
     if auth is None:
         return
     if not auth.require_auth(request.path, allowed_paths):
         return
-    if auth.authorization_header(request) == None:
+    if auth.authorization_header(request) is None:
         return abort(401)
-    if auth.current_user(request) == None:
+    if auth.current_user(request) is None:
         return abort(403)
 
 

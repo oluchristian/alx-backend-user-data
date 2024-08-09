2b0fa3ecd79ee9e9ca455273a9f2b807028eb18f
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-09 01:07:22 +0100

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""Module for API auth"""
-from flask import request
-from typing import List
+"""Module for basic auth"""
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """inherits the Auth class
     """
+    pass

990002f3e72be603e0e3b248789016d5fa82f2c2
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-09 02:31:31 +0100

    user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Module for basic auth"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple, Optional
 
 
 class BasicAuth(Auth):
@@ -36,3 +37,16 @@ class BasicAuth(Auth):
             return decoded_value
         except(base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> Tuple[Optional[str], Optional[str]]:
+        """User credentials
+        """
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        user, password = decoded_base64_authorization_header.split(":")
+        return (user, password)

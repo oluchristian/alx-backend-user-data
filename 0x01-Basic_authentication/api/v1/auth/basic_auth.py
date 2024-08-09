02267aafa87729c5a1f66267c4ea1424e4705a79
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-09 01:55:07 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,9 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """inherits the Auth class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]

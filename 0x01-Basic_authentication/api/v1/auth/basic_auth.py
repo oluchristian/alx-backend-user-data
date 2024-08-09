ca20d2db565686f6662d168eda46c3f4808495ae
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-09 02:17:54 +0100

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,9 +8,9 @@ class BasicAuth(Auth):
     """inherits the Auth class
     """
     def extract_base64_authorization_header(
-        self,
-        authorization_header: str
-        ) -> str:
+            self,
+            authorization_header: str
+            ) -> str:
         """returns base64 part
         """
         if not isinstance(authorization_header, str):
@@ -18,17 +18,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
-    
+
     def decode_base64_authorization_header(
-        self,
-        base64_authorization_header: str
-        ) -> str:
+            self,
+            base64_authorization_header: str
+            ) -> str:
         """Base64 decode
         """
         if not isinstance(base64_authorization_header, str):
             return None
         try:
-            base64_bytes = base64.b64decode(base64_authorization_header, validate=True)
+            base64_bytes = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+                )
             decoded_value = base64_bytes.decode('utf-8')
             return decoded_value
         except(base64.binascii.Error, UnicodeDecodeError):

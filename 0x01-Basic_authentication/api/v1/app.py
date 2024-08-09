ed8a0da5dafe7d4307ca1db9bdcd5e4532f6988e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-09 01:35:32 +0100

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ auth = None
 
 if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+    auth = Auth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()

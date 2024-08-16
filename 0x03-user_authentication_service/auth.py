c6075b262dcbcacd49d4b436f39baa87a1bffd12
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-16 16:21:41 +0100

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,3 +9,7 @@ import bcrypt
 def _hash_password(password: str) -> bytes:
     """nakes password with bcrpt
     """
+    encoded_pwd = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(encoded_pwd, salt)
+    return hashed_pwd

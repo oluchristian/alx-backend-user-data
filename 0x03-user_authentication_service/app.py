3fcbb015235ef9023451e4e23d3e16a500344cc3
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-17 04:39:40 +0100

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,12 @@
 """Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from user import User
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -12,6 +16,18 @@ def index():
     """
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
+
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user_exist = AUTH.register_user(email=email, password=password)
+        payload = {"email": user_exist.email, "message": "user created"}
+        return jsonify(payload), 201
+    except ValueError:
+        payload = {"message": "email already registered"}
+        return jsonify(payload), 400
 
 
 if __name__ == "__main__":

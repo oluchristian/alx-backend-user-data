91313a27b91cc73cd9d94867a992dc0df37baadc
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-17 05:19:12 +0100

    Generates session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,7 +7,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-import uuid
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -56,4 +56,15 @@ class Auth:
     def _generate_uuid() -> str:
         """Generates unique uuid
         """
-        return str(uuid.uuid4())
+        return str(uuid4())
+
+    def create_session(self, email: str) -> str:
+        """creates a session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = self._generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
